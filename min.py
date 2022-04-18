099fed589de6daf238b15a76dd450f3e4cd97ba9
kusuma <kusuma.g@thundersoft.com>
2022-04-18 05:49:06 -0700

    WAPP to find minimum nbrs in python

---
+++ min.py
@@ -0,0 +1,25 @@
+'''def minimum_nbr(givenlist):
+    mini=min(givenlist)
+    return givenlist'''
+givenlist=[100,2334,567,345,89,10]
+print(min(givenlist))
+#print(minimum_nbr(givenlist))
+#second way
+num1=int(input("enter first value :"))
+num2=int(input("enter second value :"))
+if(num1>num2):
+ print("num1 is minimum")
+else:
+ print("num2 is minimum")
+#third way
+a=500
+b=300
+if(a>b):
+   print("a=500 is minimmum",a)
+elif(a<b):
+ print("b=300 is minimum",b)
+else:
+ print("a and b are equal")
+
+
+

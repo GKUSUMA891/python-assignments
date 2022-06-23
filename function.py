cc5b68ca14b9fed8a40d48b12d03c69bf6caa59e
kusuma <kusuma.g@thundersoft.com>
2022-06-23 10:39:49 +0530

    functions

---
+++ function.py
@@ -0,0 +1,20 @@
+x=2;
+y=3;
+def add(x,y):
+    add=x+y
+    print("sum:",add)
+    return add
+add(x,y)
+def average(x,y):
+    average=(x+y)/2
+    print("average:",average)
+    return
+average(x,y)
+def power(x,y):
+    power=(x**y)
+    print("power:",power)
+    return
+power(x,y)
+
+
+

00ea33dff6345ebc575965f1b747ec5630a047c0
kusuma <kusuma.g@thundersoft.com>
2022-06-23 21:07:40 +0530

    fun program

---
+++ function1.py
@@ -0,0 +1,9 @@
+name="kusuma"; #globally create one string 
+def string_name():  #fun defination 
+    n_name=len(name); # to find length of the str
+    return
+print(name+ " is my friend")
+print(len(name))   #print str length
+print(name[2:])    # print from second index
+print(name[:2])     #print the str upto second index
+string_name()     #fun call

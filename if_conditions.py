3235b363fd3de7724a88e59bb86713dd71e16317
kusuma <kusuma.g@thundersoft.com>
2022-06-27 17:43:46 +0530

    python if statements

---
+++ if_conditions.py
@@ -0,0 +1,9 @@
+weight=input("enter weight:")
+weight_kg=int(weight)*0.40
+print("My weight is:",weight_kg)
+if(weight==190):
+    print("weight is equal")
+elif(weight!=190):
+    print("weight is not equal to value")
+elif(weight is 190):
+    print("weight is 190")

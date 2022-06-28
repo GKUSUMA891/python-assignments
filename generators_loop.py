25192a16fed6c03d88c5d1bcbac61e45d7da1a10
kusuma <kusuma.g@thundersoft.com>
2022-06-28 20:03:09 +0530

    generators usinng loops

---
+++ generators_loop.py
@@ -0,0 +1,66 @@
+def my_goal(low,high):
+    while low <=high:
+         yield low
+         low+=1
+#mine=my_goal(10,20)
+for i in my_goal(10,20):
+    print(i,end)
+
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

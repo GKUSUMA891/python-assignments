c40e2f32605cf1716815278a9b91734781afa3ea
kusuma <kusuma.g@thundersoft.com>
2022-06-30 21:13:33 +0530

    string format

---
+++ format.py
@@ -0,0 +1,11 @@
+def print_full_name(first, last):
+#print_full_name(first,last)
+    print("Hello {} {}! You just delved into python.".format(first,last))
+if __name__ == '__main__':
+    first_name = input("enter")
+    last_name = input()
+    print_full_name(first_name, last_name)
+
+    #kus
+#john
+#Hello kus john! You just delved into python.

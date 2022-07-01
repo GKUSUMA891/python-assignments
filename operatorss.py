222f8acca5ad7ca2957569b11a5cbbf53edc08e4
kusuma <kusuma.g@thundersoft.com>
2022-07-01 17:36:54 +0530

    operators

---
+++ operatorss.py
@@ -0,0 +1,36 @@
+rint(var)  #738
+
+var1=(10*2)/(12+5)
+var2=(10*2)/(12+5)*20
+print(var1)      #1.176  =>20/17 =>1.17
+print(var2)     #23.529 =>
+maxi=10+3*2**2
+print(maxi)   #22 =>10+3*4  =>10+12 =>22
+
+a=(10+3)*2**2
+print(a)     #52  =>13*4  =>52
+
+b=10+3*2**2
+print(b)    #22     =>10+3*4  =>10+12  =>22
+
+c=(2+3)*10-3
+print(c) 
+
+
+a=20
+b=30
+c=40
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+if(a==b):
+    print("a is equal to b")
+else:
+    print("a is not equal to b")
+if(a!=b):
+    print("a is not equal to b")
+else:
+     print("a is equal to b")

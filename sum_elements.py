2c69a67a661af4462fab14dfb82447c3cff4e8e7
kusuma <kusuma.g@thundersoft.com>
2022-07-06 20:13:11 +0530

    sum of elements

---
+++ sum_elements.py
@@ -0,0 +1,27 @@
+#sum of elements
+'''list1=[10,30,40,50]
+sum=0
+n=len(list1)
+for i in range(0,n):
+    sum=sum+list1[i]
+print(sum)'''
+      #-------------------
+'''sum=0
+i=0
+list1=[10,20,304,50,69]
+n=len(list1)
+while(i<n):
+    sum=sum+list1[i]
+    i+=1
+print(sum)'''
+         #--------------------
+
+'''ma=[100,200,300,400,500]
+m=sum(ma)
+print("sum of list:",m)'''
+#--------------------
+#sum of elements inside a range
+'''sum=0
+for i in range(0,20):
+    sum+=i
+    print(sum)
